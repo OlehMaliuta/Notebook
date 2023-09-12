55328cecd3d7ad68bf142c3e987943d1ae14b66c
OlehMaliuta <104268973+OlehMaliuta@users.noreply.github.com>
2023-09-12 23:45:17 +0300

    Update #41 (app process)
    * added restricting for the application to have just one process.

--- Notebook/Classes/Program.cs
+++ Notebook/Classes/Program.cs
@@ -1,14 +1,27 @@
 ﻿using MemoMates.Classes.DB;
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MemoMates
 {
     internal static class Program
     {
+        private static Mutex mutex = null;
+
         [STAThread]
         static void Main()
         {
+            const string appName = "MemoMates";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                return;
+            }
+
             using (DbApp DB = new DbApp())
             {
                 Application.EnableVisualStyles();
